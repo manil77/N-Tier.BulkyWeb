83cc2c83091490c0487dcb342c9a57ba5751294f
Manil Maharjan <manil.maharjan07@gmail.com>
2023-12-03 08:59:18 +0545

    Add project files.

---
+++ N-Tier.BulkyWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace N_Tier.BulkyWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
