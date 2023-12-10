276def398ba8dd7acae58f29d5a81410df5fd2bd
Manil Maharjan <manil.maharjan07@gmail.com>
2023-12-10 09:03:02 +0545

    Added Repository class for Category

---
+++ Bulky.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,34 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db) //Passing to the base class that is Repository.cs
+        {
+
+            _db = db;
+        }
+
+
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
