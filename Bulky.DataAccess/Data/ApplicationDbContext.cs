bb2bcbf41fe1332affd24b15c098892c7ec88c5d
Manil Maharjan <manil.maharjan07@gmail.com>
2023-12-25 09:50:21 +0545

    Working on Product CRUD

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -11,6 +11,7 @@ namespace Bulky.DataAccess.Data
         {
         }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -18,6 +19,43 @@ namespace Bulky.DataAccess.Data
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
                 new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+
+            modelBuilder.Entity<Product>().HasData(
+                 new Product
+                 {
+                     Id = 1,
+                     Title = "Game of Thrones",
+                     Description = "Book about war, politics and fantasy",
+                     ISBN = "HGAVBJAUGS75H",
+                     Author = "George R. Martin",
+                     ListPrice = 1000,
+                     Price50 = 950,
+                     Price100 = 900
+                 },
+
+                new Product
+                {
+                    Id = 2,
+                    Title = "How I Met Your Mother",
+                    Description = "Romantic slice of life story a guy named Ted Mosby",
+                    ISBN = "GHAUXGMAK875",
+                    Author = "Manil Maharjan",
+                    ListPrice = 500,
+                    Price50 = 450,
+                    Price100 = 400
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "House of Dragon",
+                    Description = "Strories of Pre events of Game of thrones",
+                    ISBN = "HJAHJX1237AK",
+                    Author = "George R. Martin",
+                    ListPrice = 700,
+                    Price50 = 650,
+                    Price100 = 600
+                }
                 );
         }
     }
---
+++ N-Tier.BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace N_Tier.BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            return View(objProductList);
+        }
+
+
+    }
+}
